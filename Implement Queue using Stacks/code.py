9b93d7e82664e1071ba868d9cff81274dae5b5b7
Artem Krutko <krutko.pn@ucu.edu.ua>
2024-04-15 15:16:08 +0300

    done queue using stack

--- Implement Queue using Stacks/code.py
+++ Implement Queue using Stacks/code.py
@@ -4,37 +4,82 @@ class Node:
         self.prev = prev
         self.next = next
 
-class MyQueue:
-
+class Stack:
     def __init__(self):
-        self.stack = Node()
+        self.top = None
 
     def push(self, x: int) -> None:
-        if self.stack.val is None:
-            self.stack.val = x
+        if self.top is None:
+            self.top = Node(x)
         else:
-            node = self.stack
-            while node.next is not None:
-                node = node.next
-            node.next = Node(x, node)
+            node = Node(x, self.top)
+            self.top.next = node
+            self.top = node
 
     def pop(self) -> int:
-        val = self.stack.val
-        self.stack = self.stack.next
-        if self.stack is not None:
-            self.stack.prev = None
+        val = self.top.val
+        self.top = self.top.prev
         return val
 
     def peek(self) -> int:
-        return self.stack.val
+        return self.top.val
 
     def empty(self) -> bool:
-        return self.stack.val is None and self.stack.next is None
+        return self.top is None
+
+class MyQueue:
+
+    # def __init__(self):
+    #     self.stack = Node()
+
+    # def push(self, x: int) -> None:
+    #     if self.stack.val is None:
+    #         self.stack.val = x
+    #     else:
+    #         node = self.stack
+    #         while node.next is not None:
+    #             node = node.next
+    #         node.next = Node(x, node)
+
+    # def pop(self) -> int:
+    #     val = self.stack.val
+    #     self.stack = self.stack.next
+    #     if self.stack is not None:
+    #         self.stack.prev = None
+    #     return val
+
+    # def peek(self) -> int:
+    #     return self.stack.val
+
+    # def empty(self) -> bool:
+    #     return self.stack is None
+
+    def __init__(self):
+        self.stack1 = Stack()
+        self.stack2 = Stack()
+
+    def push(self, x: int) -> None:
+        self.stack1.push(x)
+
+    def pop(self) -> int:
+        if self.stack2.empty():
+            while not self.stack1.empty():
+                self.stack2.push(self.stack1.pop())
+        return self.stack2.pop()
+    
+    def peek(self) -> int:
+        if self.stack2.empty():
+            while not self.stack1.empty():
+                self.stack2.push(self.stack1.pop())
+        return self.stack2.peek()
+    
+    def empty(self) -> bool:
+        return self.stack1.empty() and self.stack2.empty()
 
 
 # Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
+obj = MyQueue()
+obj.push(1)
+param_2 = obj.pop()
 # param_3 = obj.peek()
-# param_4 = obj.empty()
+param_4 = obj.empty()
